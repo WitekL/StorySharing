370290116bd71336319f5cd4e85a26dde58d1939
WitekL <witold.leicht@gmail.com>
2017-06-29 18:04:34 +0200

    Written tests for the user model. Added some validations in the user class.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,55 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Test user", email: "test@examlpe.com")
+  end
+
+  test "is it valid?" do
+    assert @user.valid?
+  end
+
+  test "name present?" do
+    @user.name = "   "
+    assert_not @user.valid?
+  end
+
+  test "mail present?" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "name length" do
+    @user.name = "a" * 51
+  end
+
+  test "email length" do
+    @user.email = "a" * 244 + "@example.com"
+  end
+
+  test "valid email addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "invalid email addresses" do
+    invalid_addresses = %w[user@example,com USER@foo..COM A_US-ER.foo.org
+                         first.last@foo_foo.jp alice+bob@baz+taz.cn]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "unique email address" do
+    user_clone = @user.dup
+    user_clone.email = @user.email.upcase
+    @user.save
+    assert_not user_clone.valid?
+  end
+
 end

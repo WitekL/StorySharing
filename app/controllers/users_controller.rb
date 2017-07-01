2cb5fcd5f7e6f37c18bb4e480d64dd8845199d70
WitekL <witold.leicht@gmail.com>
2017-07-02 01:06:41 +0200

    Created the sign-up form. Implemented the controller methods for creating a new user in the database. Added temporary styling for future development.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end

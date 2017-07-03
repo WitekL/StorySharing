e75d96f01a240a8bea1d9d443dceeefb63d925f4
WitekL <witold.leicht@gmail.com>
2017-07-03 12:56:04 +0200

    Implemented signing into user's account. Created SessionHelper and sign in form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      p "test"
+     redirect_to '/sessions/new'
     else
       render 'new'
     end
